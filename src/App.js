5d0c99d0421f96d6359ff61a59e76d73a0269dfe
Xander Krause <sk8rak@gmail.com>
2020-03-16 14:13:39 -0700

    Adding some new users

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import UserTable from './tables/UserTable'
 
 const App = () => {
+  const usersData = [
+    { id: 1, name: 'Tony Stark', username: 'tstark' },
+    { id: 2, name: 'Bruce Banner', username: 'hulk' },
+    { id: 3, name: 'Natasha Romanov', username: 'widow' },
+  ]
+
+  const [users, setUsers] = useState(usersData)
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
@@ -10,10 +19,11 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
+          <UserTable users={users} />
         </div>
       </div>
     </div>
   )
 }
 
-export default App;
+export default App
